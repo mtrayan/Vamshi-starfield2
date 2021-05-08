6d20a02b6bd74a8be34b4ac023850aeb8b5f1c2e
THANDAVARAYAN M <81505498+mtrayan@users.noreply.github.com>
2021-05-08 19:47:59 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,92 @@
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . b . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . c d c . . . . . . 
+        . . . . . . . c 5 c . . . . . . 
+        . . . . . . c d 5 d c . . . . . 
+        . . . b c c d 5 5 5 d c c b . . 
+        . . b d d 5 5 5 5 5 5 5 d d b . 
+        . . . b c c d 5 5 5 d c c b . . 
+        . . . . . . c d 5 d c . . . . . 
+        . . . . . . . c 5 c . . . . . . 
+        . . . . . . . c d c . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . b . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -50)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    mySprite2.destroy(effects.disintegrate, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 500)
+    info.changeLifeBy(-1)
+})
+let mySprite2: Sprite = null
+let Projectile2: Sprite = null
+let mySprite: Sprite = null
+effects.starField.startScreenEffect()
+info.setLife(3)
+mySprite = sprites.create(img`
+    ..........bbbbbb................
+    .......bbb444444bb..............
+    .....2244444ddd444b.............
+    ....244444444dddd44e............
+    ...244444444444ddd4be...........
+    ..244444444444444d44be..........
+    .2b444444444444444d4be..........
+    .2b44444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bbb4444444444444444bbe.........
+    2bb4b4444444444444444bbe........
+    2bb4444444444444444444be........
+    2bb44444444444444444444e........
+    2bbb444bbb4444444444444e........
+    22bbb444bb4bb444444444be........
+    .2bbbbb44bbbb44444444bbe........
+    .22bbbbbbbb44bbb444444bbe.......
+    ..eeebbbbbbb44bbb444444be.......
+    ...eeeeebbbbbbbb44b4444be.......
+    .....eeeeee222bb44bbb4bbe.......
+    .......eeeee222bb44bbbbee.......
+    ............e222bbbbbbbec.......
+    ..............ee2bbbbeebdb......
+    .................eeeeecdddb.....
+    .......................cd11bbbb.
+    ........................cd111dbb
+    .........................b11111c
+    .........................c11dd1c
+    .........................cd1dbc.
+    .........................cb11c..
+    ..........................ccc...
+    ................................
+    `, SpriteKind.Player)
+mySprite.setPosition(80, 110)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(500, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . c c c c . . . 
+        . . . . . . . c c d d d d c . . 
+        . . . . . c c c c c c d d c . . 
+        . . . c c c 4 4 4 4 d c c c c c 
+        . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+        . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+        f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+        f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+        f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+        . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+        . . f 4 4 1 4 c c 4 4 d f . . . 
+        . . . f d 4 4 4 4 4 4 c f . . . 
+        . . . . f f 4 4 4 4 c d b c . . 
+        . . . . . . f f f f d d d c . . 
+        . . . . . . . . . . c c c . . . 
+        `, SpriteKind.Enemy)
+    mySprite2.setPosition(randint(0, 160), 0)
+    mySprite2.setVelocity(0, 50)
+})
